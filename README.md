5186d63e62dd64583599ecf1e3e482b9afd2a40e
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-02-14 00:54:03 +0530

    Add some functionality

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# PRODIGY_WD_05
-It is a Weather App
+# It ia Weather App
+
+---
+
+## It is made using HTML, CSS and JavaScript
+
+---
+
+### Project Description:
+
+The weather app allows users to search for the current weather of any city in the world. The user can enter a city name into an input field and click on the "Search Icon" to get the Weather information.
+This weather app uses the OpenWeatherMap API to fetch current weather data for a given city.
+
+---
+
+### Features of the app:
+
+- The user can search for weather information by entering a city name in the input field.
+- Once the user clicks on the "Search" button, an API call will be made to retrieve weather data from openweathermap.org.
--- index.js
+++ index.js
@@ -3,10 +3,30 @@ import { API_KEY } from './config.js';
 
 
 async function checkWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=guwahati&appid=${API_KEY}&units=metric`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=siliguri&appid=${API_KEY}&units=metric`)
     var data = await response.json();
 
+    document.querySelector('.temp').innerText = Math.round(data.main.temp) + "°C";
+    document.querySelector('.city').innerText = data.name;
+    document.querySelector('.humidity').innerText = data.main.humidity + "%";
+    document.querySelector('.wind').innerText = data.wind.speed + " Km/h";
     console.log(data);
+
+    if (data.weather[0].main == "Clouds") {
+        document.querySelector('.weather-icon').src = "./images/clouds.png"
+    } else if (data.weather[0].main == "Clear") {
+        document.querySelector('.weather-icon').src = "./images/clear.png"
+    } else if (data.weather[0].main == "Drizzle") {
+        document.querySelector('.weather-icon').src = "./images/drizzle.png"
+    } else if (data.weather[0].main == "Humidity") {
+        document.querySelector('.weather-icon').src = "./images/humidity.png"
+    } else if (data.weather[0].main == "Mist") {
+        document.querySelector('.weather-icon').src = "./images/mist.png"
+    } else if (data.weather[0].main == "Rain") {
+        document.querySelector('.weather-icon').src = "./images/rain.png"
+    } else if (data.weather[0].main == "Snow") {
+        document.querySelector('.weather-icon').src = "./images/snow.png"
+    }
 }
 
 checkWeather()
