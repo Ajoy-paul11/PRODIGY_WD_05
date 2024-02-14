257ce8681d7a304510ae15782dc2cb4bd8f999a1
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-02-14 20:15:27 +0530

    Included all functions

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 import { API_KEY } from './config.js';
 
+const searchInput = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
 
+async function checkWeather(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
 
-async function checkWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=siliguri&appid=${API_KEY}&units=metric`)
-    var data = await response.json();
+    if (response.status == 404) {
+        document.querySelector('.error').style.display = 'block'
 
-    document.querySelector('.temp').innerText = Math.round(data.main.temp) + "°C";
-    document.querySelector('.city').innerText = data.name;
-    document.querySelector('.humidity').innerText = data.main.humidity + "%";
-    document.querySelector('.wind').innerText = data.wind.speed + " Km/h";
-    console.log(data);
+        document.querySelector('.temp').innerText = "NA"
+        document.querySelector('.city').innerText = "Invalid"
+        document.querySelector('.humidity').innerText = "NA"
+        document.querySelector('.wind').innerText = "NA"
 
-    if (data.weather[0].main == "Clouds") {
-        document.querySelector('.weather-icon').src = "./images/clouds.png"
-    } else if (data.weather[0].main == "Clear") {
-        document.querySelector('.weather-icon').src = "./images/clear.png"
-    } else if (data.weather[0].main == "Drizzle") {
-        document.querySelector('.weather-icon').src = "./images/drizzle.png"
-    } else if (data.weather[0].main == "Humidity") {
-        document.querySelector('.weather-icon').src = "./images/humidity.png"
-    } else if (data.weather[0].main == "Mist") {
-        document.querySelector('.weather-icon').src = "./images/mist.png"
-    } else if (data.weather[0].main == "Rain") {
-        document.querySelector('.weather-icon').src = "./images/rain.png"
-    } else if (data.weather[0].main == "Snow") {
-        document.querySelector('.weather-icon').src = "./images/snow.png"
+    } else {
+        var data = await response.json();
+
+        document.querySelector('.temp').innerText = Math.round(data.main.temp) + "°C";
+        document.querySelector('.city').innerText = data.name;
+        document.querySelector('.humidity').innerText = data.main.humidity + "%";
+        document.querySelector('.wind').innerText = data.wind.speed + " Km/h";
+        console.log(data);
+
+        if (data.weather[0].main == "Clouds") {
+            document.querySelector('.weather-icon').src = "./images/clouds.png"
+        } else if (data.weather[0].main == "Clear") {
+            document.querySelector('.weather-icon').src = "./images/clear.png"
+        } else if (data.weather[0].main == "Drizzle") {
+            document.querySelector('.weather-icon').src = "./images/drizzle.png"
+        } else if (data.weather[0].main == "Humidity") {
+            document.querySelector('.weather-icon').src = "./images/humidity.png"
+        } else if (data.weather[0].main == "Mist") {
+            document.querySelector('.weather-icon').src = "./images/mist.png"
+        } else if (data.weather[0].main == "Rain") {
+            document.querySelector('.weather-icon').src = "./images/rain.png"
+        } else if (data.weather[0].main == "Snow") {
+            document.querySelector('.weather-icon').src = "./images/snow.png"
+        }
+
+        document.querySelector('.error').style.display = 'none'
+
     }
+
 }
 
-checkWeather()
+searchBtn.addEventListener('click', function () {
+
+    checkWeather(searchInput.value)
+})
